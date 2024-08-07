ddbb6b3253a41750c32c5edf8eb1bd1ff8110d95
Johan <johan.g@academico.ifrr.edu.br>
2024-08-07 14:44:24 -0300

    Update q12.py

--- q11-20/q12.py
+++ q11-20/q12.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia o preço de um produto, calcule e mostre o seu  PREÇO PROMOCIONAL, com 5% de desconto. 
+# Questão 12: Crie um programa que leia o preço de um produto, calcule e mostre o seu  PREÇO PROMOCIONAL, com 5% de desconto. 
 preco = float(input("Digite o preço do produto: "))
 desconto = preco - (preco * 0.05)
 print(f"Com o preço promocional de 5%, o produto custará: R${round(desconto, 2)}")

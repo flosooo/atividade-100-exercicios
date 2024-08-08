99100a230c143bc3199f9849733477c9a72c04ef
Johan <johan.g@academico.ifrr.edu.br>
2024-08-08 11:09:06 -0300

    Create q24.py

---
+++ q21-q30/q24.py
@@ -0,0 +1,6 @@
+# Questão 24: Faça um algoritmo que pergunte a distância que um passageiro deseja
+# percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para
+# viagens até 200Km e R$0.45 para viagens mais longas.
+distancia = float(input("Digite a distância que deseja percorrer em Km: "))
+preco = distancia * 0.45 if distancia > 200 else distancia * 0.5
+print(f"O preço da passagem é R${preco:.3f}")

cd197c8cbd802d04752070973591e871527c685f
Johan <johan.g@academico.ifrr.edu.br>
2024-08-08 14:24:22 -0300

    Update q39.py

--- q31-q40/q39.py
+++ q31-q40/q39.py
@@ -1,8 +1,5 @@
 # Questão 39: Faça um algoritmo que mostre na tela a seguinte contagem:
 # 10 9 8 7 6 5 4 3 Acabou!
-import time
-
 for i in range(10, 2, -1):
   print(i)
-  time.sleep(.5)
 print('Acabou!')

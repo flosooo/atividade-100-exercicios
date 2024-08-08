cad743b285689eba0c10b3f65a939c2459f05dee
Johan <johan.g@academico.ifrr.edu.br>
2024-08-08 14:24:35 -0300

    Update q38.py

--- q31-q40/q38.py
+++ q31-q40/q38.py
@@ -1,8 +1,5 @@
 # Questão 38: Escreva um programa que mostre na tela a seguinte contagem:
 # 6 7 8 9 10 11 Acabou!
-import time
-
 for i in range(6, 12):
   print(i)
-  time.sleep(.5)
 print('Acabou!')
